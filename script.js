20edb71c99d73c8e07d115984dd36be30a4e4869
PritamKumarSaha <137249952+PritamKumarSaha@users.noreply.github.com>
2024-01-18 19:28:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ function Person(name, age) {
 };
 // adding a method to the Person prototype
 Person.prototype.greet = function() {
-	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
+	console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`)
 };
 
-
+// Constructor function for employee
 function Employee(name, age, jobTitle) {
+	// Calling the Person constructor to initialize name and age
 	Person.call(this, name, age);
 	this._jobTitle = jobTitle;
 };
+// 
 Employee.prototype = Object.create(Person.prototype);
 
 Employee.prototype.jobGreet = function() {
